d5769e7626f106d3c530ff8683e4566a57178214
Perelyn-sama <okohebina@gmail.com>
2022-09-23 14:23:22 +0100

    added comments

--- src/playground.rs
+++ src/playground.rs
@@ -1,17 +1,17 @@
-use ethers::core::utils::keccak256;
-use ethers::types::H256;
+// use ethers::core::utils::keccak256;
+// use ethers::types::H256;
 
 // I come here to mess around with things
 pub fn run() {
     // Charset::from_str('s');
-    println!("{:?}", H256::from(keccak256("Perelyn".as_bytes())));
+    // println!("{:?}", H256::from(keccak256("Perelyn".as_bytes())));
     // println!("{:?}", "Perelyn".as_bytes());
 
-    println!(
-        "{:?}",
-        "0xeadb26545b61ffc09fc97b674bad79e3af067ac581b7f124a622bb976b128759"
-            == "0xeadb26545b61ffc09fc97b674bad79e3af067ac581b7f124a622bb976b128759"
-    )
+    // println!(
+    //     "{:?}",
+    //     "0xeadb26545b61ffc09fc97b674bad79e3af067ac581b7f124a622bb976b128759"
+    //         == "0xeadb26545b61ffc09fc97b674bad79e3af067ac581b7f124a622bb976b128759"
+    // )
 }
 // 0xeadb26545b61ffc09fc97b674bad79e3af067ac581b7f124a622bb976b128759 Perelyn js
 // toUtf8 js Perelyn Uint8Array(7) [ 80, 101, 114, 101, 108, 121, 110 ]
