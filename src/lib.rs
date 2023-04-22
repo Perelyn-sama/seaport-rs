10f144be7c0d19b326fc00aadfa7d716211ffd03
Perelyn <pisckid344@gmail.com>
2023-04-22 12:16:57 +0100

    add prelude

--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,13 @@ pub mod bindings;
 pub mod constants;
 pub mod seaport;
 pub mod types;
+
+pub mod prelude {
+    pub use super::{
+        bindings::seaport::{Order, OrderComponents},
+        constants,
+        constants::CROSS_CHAIN_SEAPORT_ADDRESS,
+        seaport::Seaport,
+        types::{CreateOrderInput, SeaportConfig},
+    };
+}
